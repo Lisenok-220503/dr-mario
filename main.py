090786ff0faaf6477ac691b69362d03dbf2f1bca
zix999 <45551524+zix999@users.noreply.github.com>
2019-01-14 10:46:26 +0300

    created main as a main game cycle

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+size = width, height = 600, 450
+screen1 = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for i in pygame.event.get:
+        if event.type == pygame.QUIT:
+            running = False
+
+    
