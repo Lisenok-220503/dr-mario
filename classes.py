fd85a3d5b26783eab1d531db5c61487bb9227b6f
zix999 <45551524+zix999@users.noreply.github.com>
2019-01-14 09:17:49 +0300

    added destruction method for pill class

--- classes.py
+++ classes.py
@@ -3,12 +3,20 @@ class Object():
         self.color = color
         self.size = width, height = 30, 30
 
+    def destruction(self):
+        global points
+        points += 50
+
 class Pill(Object):
     def __init__(self, color1, color2):
         self.color = [color1, color2]
         self.size = width, height = 30, 30
+    def destruction(self):
+        super().destruction()
 
 class Virus(Object):
     def __init__(self, color):
         super().__init__()
-        
+
+
+
