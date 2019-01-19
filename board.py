303cbbdb75008d05648b6a447db18c327eb2a108
Lisenok-220503 <you@example.comlisenok220503@yandex.ru>
2019-01-19 15:24:22 +0300

    update
    added method start_spawn

--- board.py
+++ board.py
@@ -1,14 +1,21 @@
 import pygame
+import random
+from classes import Virus
 
 class Board:
     
     def __init__(self):
         self.width = 250
         self.height = 450
-        self.board = [[0] * 250 for _ in range(450)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+        self.display = pygame.Surface((240, 450))
+        self.board = []
+        for h in range(0, 16):
+            self.board.append([])
+            for w in range(0, 9):
+                self.board[h].append(Object(w, h))
+        #self.left = 10
+        #self.top = 10
+        #self.cell_size = 30
         
     
     def set_view(self, left, top, cell_size):
@@ -16,9 +23,9 @@ class Board:
         self.top = top
         self.cell_size = cell_size
     
-    def start_draw(self):
+    def start_spawn(self):
         for i in range(6):
             self.virus = Virus()
-            
+    
         
     
