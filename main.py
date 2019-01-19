014c4f33c84a8ae0d76036c046752a9f09710358
Lisenok-220503 <you@example.comlisenok220503@yandex.ru>
2019-01-19 21:51:24 +0300

    update
    Co-Authored-By: lisenok-220503 <lisenok-220503@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import pygame
 from board import Board
+from classes import Background
 
 pygame.init()
 
 total_width = 900
 total_height = 650
 
-image = load_image("image.jpg")
-
-
 pygame.mixer.music.load("music.mp3") 
 pygame.mixer.music.play(-1,0.0)
+
+clock = pygame.time.Clock()
+
+BackGround = Background('background_image.png', [0,0])
+
+
+running = True
 
 def init_window():
     pygame.init()
     screen1 = pygame.display.set_mode((total_width, total_heigth))
     pygame.display.set_caption('Dr Mario')
-
-clock = pygame.time.Clock()
-
-running = True
-
+    
+    
 while running:
     for i in pygame.event.get:
         if event.type == pygame.QUIT:
