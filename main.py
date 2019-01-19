d6aad95039463d72b7df5ff7c0eba5f813a8f242
Lisenok-220503 <you@example.comlisenok220503@yandex.ru>
2019-01-19 18:56:45 +0300

    update
    added background music

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from board import Board
 
-window_size = total_width, total_height = 900, 650
+total_width = 900
+total_height = 650
+
+pygame.mixer.music.load("music.mp3") 
+pygame.mixer.music.play(-1,0.0)
 
 def init_window():
     pygame.init()
-    screen1 = pygame.display.set_mode(window_size)
+    screen1 = pygame.display.set_mode((total_width, total_heigth))
     pygame.display.set_caption('Dr Mario')
 
 clock = pygame.time.Clock()
