92f4de557c1c97c878cabc03b671fb1085c9852e
mint_ <ya.maxkuks@yandex.ru>
2019-01-19 23:19:31 +0300

    fixed some issues

--- board.py
+++ board.py
@@ -5,33 +5,30 @@ from classes import Object, Virus, Pill
 spawn_pos = (110, 0)
 class Board:
     
-    def __init__(self):
+    def __init__(self,  left, top):
         self.width = 250
         self.height = 450
+        self.left = left
+        self.top = top
         self.display = pygame.Surface((240, 450))
         self.board = []
         for h in range(0, 16):
             self.board.append([])
             for w in range(0, 9):
                 self.board[h].append(Object(w, h))
-        #self.left = 10
-        #self.top = 10
-        #self.cell_size = 30
-        
-    
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
+        # self.left = 10
+        # self.top = 10
+        self.cell_size = 30
     
     def start_spawn_virus(self):
+        self.virus_list = []
         for i in range(6):
-            self.virus = Virus()
-            self.virus.draw(self.display, (randrange(0, 241), randrange(225, 451)))
+            self.virus_list.append(Virus())
+            self.virus.draw(self.display, (randrange(0, 241, 30), randrange(225, 451, 30)))
     
     def spawn_pill(self):
         pill = Pill()
-        pill.draw(self.display, ((110, 0), (30, 30)), self.size, self.size)
+        pill.draw(self.display, ((110, 0), (30, 30)), self.cell_size, self.cell_size)
         
     
         
