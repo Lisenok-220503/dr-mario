86935c17dfc9ea81ba8d4c9414feb5a54d811269
Lisenok-220503 <lisenok220503@yandex.ru>
2019-01-27 09:48:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ from classes import Background
 total_width = 900
 total_height = 650
 
-music = pygame.mixer.music.load("fever.mp3")
-pygame.mixer.music.play(-1, 0.0)
-
-clock = pygame.time.Clock()
-
 # Каждые полторы секунды пилюля автоматически опускается.
 # Для этого создаю свой ивент.
 # В главной переменной я хз, что забивать. Пусть будет как в примере. 	¯\_(ツ)_/¯
 
-BackGround = Background('background.jpg', [0,0])
 
 running = True
 game = False
 
 class game:
     def __init__(self):
-        self.board = Board()
         pygame.init()
+        
+        self.music = pygame.mixer.music.load("fever.mp3")
+        pygame.mixer.music.play(-1, 0.0)
+        
+        self.BackGround = Background('background.jpg', [0,0])
+        
+        clock = pygame.time.Clock()
 
         self.fpsClock = pygame.time.Clock()
         self.screen1 = pygame.display.set_mode((total_width, total_height))
@@ -35,29 +35,29 @@ class game:
         pygame.time.set_timer(self.time_event, 1500)        
 
     while running:
-        screen1.fill([255, 255, 255])
-        screen1.blit(BackGround.image, BackGround.rect)
+        self.screen1.fill([255, 255, 255])
+        self.screen1.blit(BackGround.image, BackGround.rect)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
             if event.type == pygame.K_RETURN:
-                board = Board()
+                self.board = Board()
                 game = True
             if event.type == pygame.K_SPACE and game:
-                board.rotate()
+                self.board.rotate()
 
             if event.type == pygame.K_RIGHT and game:
-                board.move_right()
+                self.board.move_right()
 
             if event.type == pygame.K_LEFT and game:
-                board.move_left()
+                self.board.move_left()
 
             if (event.type == pygame.K_DOWN or event.type == time_event) and game:
-                board.move_down()
-        board.check()
+                self.board.move_down()
+        self.board.check()
         #board.render()
-        screen1.blit(BackGround.image, BackGround.rect)
+        self.screen1.blit(self.BackGround.image, self.BackGround.rect)
         pygame.display.flip()
 
 
